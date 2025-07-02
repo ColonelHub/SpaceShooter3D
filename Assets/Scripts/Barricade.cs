c6345244181013c02066655a2352fc5a8d3602b7
Ignacio Fernandez Lemos <ignacio.f.l@hotmail.com>
2025-07-02 13:01:29 -0300

    - Added plasma shader health to barricades

--- Assets/Scripts/Barricade.cs
+++ Assets/Scripts/Barricade.cs
@@ -4,15 +4,27 @@ public class Barricade : MonoBehaviour
 {
     [SerializeField] private int currentHealth;
     [SerializeField] private int maxHealth;
+    [SerializeField] private Renderer meshRenderer;
+
+    [GradientUsage(true)]
+    [SerializeField] private Gradient hdrGradient;
 
     void Start()
     {
         currentHealth = maxHealth;
+        UpdateColor();
+    }
+
+    private void UpdateColor()
+    {
+        float value = 1 - ((float)currentHealth / (float)maxHealth);
+        meshRenderer.material.color = hdrGradient.Evaluate(value);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         currentHealth--;
+        UpdateColor();
         Destroy(other.gameObject);
         if (currentHealth <= 0)
         {
